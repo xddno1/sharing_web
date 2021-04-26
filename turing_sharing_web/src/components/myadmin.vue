b0f423e2f19e58f7dc97325add821c200c92c9b0
xddno1 <2196815716@qq.com>
2021-04-27 02:07:09 +0800

    1

--- turing_sharing_web/src/components/myadmin.vue
+++ turing_sharing_web/src/components/myadmin.vue
@@ -1,34 +1,57 @@
 <template>
   <div class="myadmin">
     <div class="pages">
-      <div>姓名：<input type="text" v-model="myname" /></div>
-      <div>头像：<input type="file" ref="file" /></div>
-      <button @click="save">a</button>
+      <h1>图片上传</h1>
+      <div>
+        <span>请输入文章id</span>
+        <input type="text" v-model="passageid" />
+      </div>
+
+      <input
+        id="inputElement"
+        name="file"
+        ref="file"
+        type="file"
+        accept="image/png, image/gif, image/jpeg"
+      />
+      <button @click="uploadimg">上传</button>
     </div>
+    <div></div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "myadmin",
   components: {},
   props: [],
   data() {
     return {
-      myname,
+      file: null,
+      myname: "",
+      passageid: "",
     };
   },
   methods: {
-    save() {
+    uploadimg() {
       let formData = new FormData();
       console.log(formData);
-      formData.append("name", this.name);
-      formData.append("img", this.$refs.file.files[0]);
-      this.axios
-        .post("/api/user/query", formData, {
+      formData.append("passageID", this.passageid);
+      console.log(formData.get("passageID"));
+      formData.append("file", this.$refs.file.files[0]);
+      console.log(formData.get("file"));
+      axios({
+        method: "post",
+        url: "http://121.4.187.232:8080/admin/uploadImg",
+        data: formData,
+        headers: {
           "Content-Type": "multipart/form-data",
-        })
-        .then((res) => {});
+          token: this.$store.state.loginstate.admintoken,
+        },
+      }).then((e) => {
+        this.$message.success("上传成功！");
+      });
     },
   },
   created() {
